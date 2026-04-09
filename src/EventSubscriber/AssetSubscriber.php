ce7bd629f859631b2de0cbefdb745f7dc628ba65
Marko Cupic <m.cupic@gmx.ch>
2026-04-09 20:33:57 +0200

    development

--- src/EventSubscriber/AssetSubscriber.php
+++ src/EventSubscriber/AssetSubscriber.php
@@ -19,10 +19,12 @@ use Symfony\Component\Asset\Packages;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
 
 class AssetSubscriber implements EventSubscriberInterface
 {
     public function __construct(
+        private readonly Environment $twig,
         private readonly Packages $packages,
         private readonly ScopeMatcher $scopeMatcher,
     ) {
@@ -37,16 +39,15 @@ class AssetSubscriber implements EventSubscriberInterface
     {
         $request = $e->getRequest();
 
+        // Favicon
+        $GLOBALS['TL_HEAD'][] = $this->twig->render('@MarkocupicContaoSchuldiensteTheme/favicon.html.twig');
+
         if ($this->scopeMatcher->isFrontendRequest($request)) {
             // Add Font Awesome
             $GLOBALS['TL_HEAD'][] = '<link rel="stylesheet" href="assets/contao-component-fontawesome-free/fontawesomefree/css/all.css">';
 
             // Add Google Fonts
-            $GLOBALS['TL_HEAD'][] = '
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght,GRAD@0,17..18,400..700,-50..200;1,17..18,400..700,-50..200&display=swap" rel="stylesheet">
-';
+            $GLOBALS['TL_HEAD'][] = $this->twig->render('@MarkocupicContaoSchuldiensteTheme/google_fonts.html.twig');
 
             // Add theme assets
             $GLOBALS['TL_HEAD'][] = '<script src="'.$this->packages->getUrl('js/theme.js', 'markocupic_contao_schuldienste_theme').'"></script>';

7ff15a209f396fb29a2b7d4ede68d928405edc3d
Marko Cupic <m.cupic@gmx.ch>
2026-06-20 19:49:16 +0200

    Change the fill color of the grey boxes in the logo to darkgrey

--- webpack.config.js
+++ webpack.config.js
@@ -8,24 +8,29 @@ Encore
     //.addEntry('select2', './assets/entries/select2.js')
     //.addEntry('frontend', './assets/filepond.js')
     .copyFiles({
-        from: './node_modules/bootstrap/dist/js',
-        to: 'bootstrap/dist/js/[path][name].[ext]',
-        pattern: /(bootstrap\.bundle\.min\.js)$/,
+      from: './node_modules/bootstrap/dist/js',
+      to: 'bootstrap/dist/js/[path][name].[ext]',
+      pattern: /(bootstrap\.bundle\.min\.js)$/,
     })
 
     .copyFiles({
-        from: './assets/js',
-        to: 'js/[path][name].[hash:8].[ext]'
+      from: './assets/js',
+      to: 'js/[path][name].[hash:8].[ext]'
     })
 
     .copyFiles({
-        from: './assets/images',
-        to: 'images/[path][name].[ext]'
+      from: './assets/images/icons',
+      to: 'images/icons/[path][name].[ext]'
     })
 
     .copyFiles({
-        from: './assets/images/logos',
-        to: 'images/logos/[path][name].[hash:8].[ext]'
+      from: './assets/images/loginform',
+      to: 'images/loginform/[path][name].[ext]'
+    })
+
+    .copyFiles({
+      from: './assets/images/logos',
+      to: 'images/logos/[path][name].[hash:8].[ext]'
     })
 
     .disableSingleRuntimeChunk()
@@ -35,8 +40,8 @@ Encore
 
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
+      config.useBuiltIns = 'usage';
+      config.corejs = 3;
     })
 
     .enablePostCssLoader()
